b27909b60351111a7ce20125d65a11d628f45af5
Mohamed-Shahata <mohamedmrslan@gmail.com>
2025-04-22 20:18:35 -0100

    update

--- src/pages/EditProfile.js
+++ src/pages/EditProfile.js
@@ -83,9 +83,7 @@ function EditProfile() {
       const formData = new FormData();
       formData.append('user-image', newAvatar);
 
-      const response = await api.post('/users/images/upload-image', formData,
-        { withCredentials: true }
-      );
+      const response = await api.post('/users/images/upload-image', formData);
 
       const newImageUrl = response.data.imageUrl;
       if (!newImageUrl) {
