d532428634780e0f20e44f56c40aadff8f3c8416
Mohamed Shahata <125826170+Mohamed-Shahata@users.noreply.github.com>
2025-04-23 23:55:37 +0200

    Update Profile.js

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -688,7 +688,12 @@ const Profile = () => {
       </Flex>
       <VStack align="start" spacing={4} mb={6}>
         <Text>{profileUser?.bio || ''}</Text>
-        <Divider borderColor="gray.400" borderWidth="1px" my={2} />
+        {profileUser?.bio && (
+  <>
+    <Text>{profileUser.bio}</Text>
+    <Divider borderColor="gray.400" borderWidth="1px" my={2} />
+  </>
+)}
         {profileUser?.occupation && (
           <Text fontWeight="semibold" color="cyan.700">
             {profileUser.occupation}
