a03bfa7b4086a818e7ae640266c1f373f0213422
Mohamed-Shahata <mohamedmrslan@gmail.com>
2025-04-23 15:21:55 -0100

    update

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,4 +1,4 @@
-import { Box, Button, FormControl, FormLabel, Heading, Input, Text, Link, useColorModeValue, useToast } from '@chakra-ui/react'
+import { Box, Button, FormControl, FormLabel, Heading, Input, Select, Text, Link, useColorModeValue, useToast, SimpleGrid } from '@chakra-ui/react'
 import React, { useState } from 'react'
 import { Link as RouterLink, useNavigate } from "react-router-dom"
 import Cookies from "js-cookie"
@@ -9,6 +9,7 @@ function Register() {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [age, setAge] = useState('');
+  const [gender, setGender] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
@@ -22,7 +23,7 @@ function Register() {
     setIsLoading(true);
     try {
       const response = await api.post(`/auth/register`, {
-        firstName, lastName, age, email, password
+        firstName, lastName, age, gender, email, password
       });
 
       if (response.status === 200) {
@@ -48,9 +49,10 @@ function Register() {
     }
     setIsLoading(false);
   }
+
   return (
     <Box
-      maxW="400px"
+      maxW="600px"
       mx="auto"
       mt="100px"
       p={6}
@@ -59,65 +61,90 @@ function Register() {
       boxShadow="lg"
       bg={bg}
       borderColor={borderColor}
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
     >
       <Heading mb={6} textAlign="center">AQ</Heading>
 
-      <form onSubmit={handleSubmit}>
-        <FormControl mb={4}>
-          <FormLabel>First Name</FormLabel>
-          <Input
-            type='text'
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-            placeholder='Enter your first name please'
-            required
-          />
-        </FormControl>
-        <FormControl mb={4}>
-          <FormLabel>Last Name</FormLabel>
-          <Input
-            type='text'
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-            placeholder='Enter your last name please'
-            required
-          />
-        </FormControl>
-        <FormControl mb={4}>
-          <FormLabel>Age</FormLabel>
-          <Input
-            type="number"
-            value={age}
-            onChange={(e) => setAge(Number(e.target.value))}
-            placeholder='Enter your age please'
-            required
-          />
-        </FormControl>
-        <FormControl mb={4}>
-          <FormLabel>Email</FormLabel>
-          <Input
-            type='email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder='Enter your email please'
-            required
-          />
-        </FormControl>
-        <FormControl mb={4}>
-          <FormLabel>Password</FormLabel>
-          <Input
-            type='password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder='Enter your password please'
-            required
-          />
-        </FormControl>
-        <Button colorScheme='teal' width="full" type="submit" isLoading={isLoading}> Create Account</Button>
+      <form onSubmit={handleSubmit} style={{ width: '100%' }}>
+        <SimpleGrid columns={{ base: 1, md: 2 }} spacing={4}>
+          <FormControl>
+            <FormLabel>First Name</FormLabel>
+            <Input
+              type='text'
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              placeholder='Enter your first name please'
+              required
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Last Name</FormLabel>
+            <Input
+              type='text'
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              placeholder='Enter your last name please'
+              required
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Age</FormLabel>
+            <Input
+              type="number"
+              value={age}
+              onChange={(e) => setAge(Number(e.target.value))}
+              placeholder='Enter your age please'
+              required
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Gender</FormLabel>
+            <Select
+              value={gender}
+              onChange={(e) => setGender(e.target.value)}
+              placeholder='Select gender'
+              required
+            >
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+            </Select>
+          </FormControl>
+          <FormControl>
+            <FormLabel>Email</FormLabel>
+            <Input
+              type='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder='Enter your email please'
+              required
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Password</FormLabel>
+            <Input
+              type='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder='Enter your password please'
+              required
+            />
+          </FormControl>
+        </SimpleGrid>
+        <Button
+          colorScheme='teal'
+          width="full"
+          type="submit"
+          isLoading={isLoading}
+          mt={6}
+        >
+          Create Account
+        </Button>
       </form>
       <Text mt={4} textAlign="center">
-        Already have account
-        <Link as={RouterLink} to="/login" color="teal.500"> Login</Link>
+        Already have account?{' '}
+        <Link as={RouterLink} to="/login" color="teal.500">Login</Link>
       </Text>
     </Box>
   )
